4b386c4ac5b1b329c62173626c4c08f99170a443
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-09-20 12:18:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,9 +23,9 @@ HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
 for item in re.split('], |].', input_data):
-  item = item[0:]
+  item = item[1:]
   data = item.split(' , ')
   if len(data) > 1:
-    insert(HashTable, int(data[0]), data[0])
+    insert(HashTable, int(data[0]), data[1])
 
 display_hash (HashTable)
