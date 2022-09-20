6d879cb4d3e905a4ca53ed9a8adb55a0a37ceb17
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-09-20 10:55:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ input_data = input()
 data = []
 for item in re.split('], |].', input_data):
   item = item[1:]
-  data = item.split(', ')
+  data = item.split(', ') 
   if len(data) > 1:
     insert(HashTable, int(data[0]), data[1])
 
