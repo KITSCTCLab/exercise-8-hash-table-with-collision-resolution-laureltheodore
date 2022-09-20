4948a616e28f6f224c76cd29d864fbc3b1d11041
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-09-20 11:43:14 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,7 +21,7 @@ hash_table_size = int(input())
 # Create Hashtable as a list of list.
 HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
-data = []
+data = [] 
 for item in re.split('], |].', input_data):
   item = item[1:]
   data = item.split(', ') 
